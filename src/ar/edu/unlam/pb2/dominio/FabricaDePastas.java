79359c3ae1931e5f93e720e1bd49fedbbe287093
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2024-08-21 10:17:15 -0300

    Agrego un nuevo test para eliminar un elemento

--- src/ar/edu/unlam/pb2/dominio/FabricaDePastas.java
+++ src/ar/edu/unlam/pb2/dominio/FabricaDePastas.java
@@ -7,24 +7,27 @@ public class FabricaDePastas {
 	// Coleccion: elemento que contiene muchos elementos.
 	// ArrayList
 
-	//private Pasta[] pasta;
+	// private Pasta[] pasta;
 	private ArrayList<Pasta> pastas;
 
 	public FabricaDePastas() {
-		//this.pasta = new Pasta[5];
+		// this.pasta = new Pasta[5];
 		this.pastas = new ArrayList<>();
-
 	}
 
 	public Boolean agregar(Pasta pasta) {
-		//int -> Interger
-		//long -> Long
-		//boolean -> Boolean (Puede decir null y no solo true/false)
+		// int -> Interger
+		// long -> Long
+		// boolean -> Boolean (Puede decir null y no solo true/false)
 		return this.pastas.add(pasta);
 	}
 
 	public ArrayList<Pasta> obtenerPastas() {
 		return this.pastas;
+	}
+
+	public Boolean eliminar(Pasta pasta) {
+		return this.pastas.remove(pasta);
 	}
 
 }
--- test/ar/edu/unlam/pb2/dominio/FabricaDePastasTest.java
+++ test/ar/edu/unlam/pb2/dominio/FabricaDePastasTest.java
@@ -1,6 +1,5 @@
 package ar.edu.unlam.pb2.dominio;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -11,40 +10,61 @@ import org.junit.Test;
 
 public class FabricaDePastasTest {
 
-	//Registrar pasta: Agrega una pasta a la coleccion de pastas disponibles para agregar a un pedido
-	
+	// Registrar pasta: Agrega una pasta a la coleccion de pastas disponibles para
+	// agregar a un pedido
+
 	private FabricaDePastas fabricaDePastas;
-	
+
 	@Before
 	public void init() {
-		this.fabricaDePastas = new FabricaDePastas()
-;	}
+		this.fabricaDePastas = new FabricaDePastas();
+	}
+
 	@Test
 	public void dadoQueExisteUnaFabricaDePastasCuandoAgregoUnaPastaALaColeccionDePastasDisponiblesLaColeccionContieneLaPasta() {
-		//Preparacion:
+		// Preparacion:
 		Pasta pasta = new Pasta();
-		
-		//Ejecucion:
+
+		// Ejecucion:
 		Boolean pastaAgregada = this.fabricaDePastas.agregar(pasta);
-		
-		//Verificacion: 
+
+		// Verificacion:
 		assertTrue(pastaAgregada);
 	}
-	
+
 	@Test
 	public void dadoQueExisteUnaFabricaDePastasCon2PastasCuandoObtengoLaColeccionObtenidaContiene2Elementos() {
-		//Preparacion:
+		// Preparacion:
 		Pasta ravioles = new Pasta();
 		Pasta agnolotis = new Pasta();
 		this.fabricaDePastas.agregar(ravioles);
 		this.fabricaDePastas.agregar(agnolotis);
 
-		//Ejecucion:
+		// Ejecucion:
 		ArrayList<Pasta> pastasObtenidas = this.fabricaDePastas.obtenerPastas();
-		
-		//Verificacion: 
+
+		// Verificacion:
 		int cantidadEsperada = 2;
-		
+
+		assertEquals(cantidadEsperada, pastasObtenidas.size());
+	}
+
+	@Test
+	public void dadoQueExisteUnaFabricaDePastasCon2PastasCuandoElimino1LaColeccionObtenidaContiene1Elemento() {
+		// Preparacion:
+		Pasta ravioles = new Pasta();
+		Pasta agnolotis = new Pasta();
+		this.fabricaDePastas.agregar(ravioles);
+		this.fabricaDePastas.agregar(agnolotis);
+
+		this.fabricaDePastas.eliminar(agnolotis);
+
+		// Ejecucion:
+		ArrayList<Pasta> pastasObtenidas = this.fabricaDePastas.obtenerPastas();
+
+		// Verificacion:
+		int cantidadEsperada = 1;
+
 		assertEquals(cantidadEsperada, pastasObtenidas.size());
 	}
 }
